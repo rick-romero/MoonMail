7b7c6e6e74e157fc8796dbfa048951b659668ea0
Rhino <rhinoandre@gmail.com>
2018-10-08 13:26:17 -0300

    [4519] Unit tests for Handler

--- campaigns-microservice-new/src/api/createCampaign/handler.spec.ts
+++ campaigns-microservice-new/src/api/createCampaign/handler.spec.ts
@@ -1,49 +1,91 @@
 'use strict';
 
-import respond from './handler';
-import * as CampaignModel from '../../models/schema';
+import { handlerService } from './handler';
+import { CampaignRepository } from '../../types';
+import * as decryptor from '../../lib/auth-token-decryptor';
 
 describe('createCampaign', () => {
+  let campaignService: CampaignRepository;
+  let handler;
 
-  const campaign = {
-    senderId: 'ca654',
-    subject: 'my campaign subject',
-    listIds: 'ca43546',
-    name: 'my campaign',
-    body: 'my campaign body'
-  };
+  beforeEach(() => {
+    campaignService = {
+      save: jest.fn(),
+      edit: jest.fn(),
+      get: jest.fn(),
+      list: jest.fn()
+    };
+    handler = handlerService(campaignService);
+  });
 
-  describe('#respond()', () => {
-    beforeEach(() => {
-      
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  it('should creates the campaign when the event is valid', async () => {
+    // GIVEN
+    spyOn(decryptor, 'default').and.returnValue({sub: 'my-user-id'});
+
+    // WHEN
+    const result = await handler({
+      campaign: {
+        senderId: 'ca654',
+        segmentId: 'anotherId',
+        subject: 'my campaign subject',
+        listId: 'ca43546',
+        name: 'my campaign',
+        body: 'my campaign body',
+        template: 'htmlBody',
+        isUpToDate: true,
+        sentAt: 0,
+        createdAt: 0,
+        scheduleAt: 0
+      },
+      authToken: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5UWTJSRGt4TVVJeE9FRTFNRFZET0Rnd05VWTVORUUxT0RSQk1qaEJSakkxTkVNd1FrSTBNdyJ9.eyJpc3MiOiJodHRwczovL21vb25tYWlsLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwODMyODMwMzkxNDA0Mzc2NTUxMyIsImF1ZCI6Inl4YUhSVm1BbFp2UFM5bXdTYnd6bXk5WTI0aDhtS2NWIiwiaWF0IjoxNTM4OTkwMjUzLCJleHAiOjE1Mzg5OTM4NTMsImF0X2hhc2giOiJEWGJHSE1OdHhZX0JOSkxDTGpKMWRRIiwibm9uY2UiOiJ5eGFIUlZtQWxadlBTOW13U2J3em15OVkyNGg4bUtjViJ9.zq-476NXJzwdOe_bzCEzIuh266TM-4oS18Xv_UHacHQ5GOyTWRBWkUToZGulDslEVBPgfDaqFEmolyrymMYQJ8cWqI561QIvUszjSvV9ENYzf72gSTnNYDX8RnvZIt6_3obDduByEBvUEdSSm6fZDh8yVx_BQeq_ItSNBP8pT4wmh1Glv9qqi9W9FifaegErGv8rdVMJL_jHhHnqIWxspxzkhMhbXKkL5q0XSnOv5kDFvHocuOvqjMUb488lS8L_PcHiovnDF0PzMPwc1mmOhVZGOuFvEtVCzHpZrw-ySc3KyGdX8ogfTZ4YRnxYGLriI8-hoDZVZk-pfFBnjobDKQ'
     });
 
-    context('when the event is valid', () => {
-      it('creates the campaign', (done) => {
-        respond(event, (err, result) => {
-          const args = CampaignModel.save.firstCall.args[0];
-          expect(args).to.have.property('userId');
-          expect(args).to.have.property('id');
-          expect(err).to.not.exist;
-          expect(result).to.exist;
-          done();
-        });
+    // THEN
+    expect(result.userId).toBe('my-user-id');
+    expect(result.status).toBe('draft');
+
+    // AND
+    expect(campaignService.save).toHaveBeenCalledWith({
+      userId: 'my-user-id',
+      senderId: 'ca654',
+      segmentId: 'anotherId',
+      subject: 'my campaign subject',
+      listId: 'ca43546',
+      name: 'my campaign',
+      body: 'my campaign body',
+      template: 'htmlBody',
+      isUpToDate: true,
+      status: 'draft',
+      sentAt: 0,
+      createdAt: 0,
+      scheduleAt: 0
+    });
+    expect(decryptor.default).toHaveBeenCalledWith('Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik5UWTJSRGt4TVVJeE9FRTFNRFZET0Rnd05VWTVORUUxT0RSQk1qaEJSakkxTkVNd1FrSTBNdyJ9.eyJpc3MiOiJodHRwczovL21vb25tYWlsLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwODMyODMwMzkxNDA0Mzc2NTUxMyIsImF1ZCI6Inl4YUhSVm1BbFp2UFM5bXdTYnd6bXk5WTI0aDhtS2NWIiwiaWF0IjoxNTM4OTkwMjUzLCJleHAiOjE1Mzg5OTM4NTMsImF0X2hhc2giOiJEWGJHSE1OdHhZX0JOSkxDTGpKMWRRIiwibm9uY2UiOiJ5eGFIUlZtQWxadlBTOW13U2J3em15OVkyNGg4bUtjViJ9.zq-476NXJzwdOe_bzCEzIuh266TM-4oS18Xv_UHacHQ5GOyTWRBWkUToZGulDslEVBPgfDaqFEmolyrymMYQJ8cWqI561QIvUszjSvV9ENYzf72gSTnNYDX8RnvZIt6_3obDduByEBvUEdSSm6fZDh8yVx_BQeq_ItSNBP8pT4wmh1Glv9qqi9W9FifaegErGv8rdVMJL_jHhHnqIWxspxzkhMhbXKkL5q0XSnOv5kDFvHocuOvqjMUb488lS8L_PcHiovnDF0PzMPwc1mmOhVZGOuFvEtVCzHpZrw-ySc3KyGdX8ogfTZ4YRnxYGLriI8-hoDZVZk-pfFBnjobDKQ');
+  });
+
+  it('should raise an error if the auth token is not passed', async () => {
+    // GIVEN
+    spyOn(decryptor, 'default').and.callThrough();
+
+    // WHEN
+    try {
+      await handler({
+        campaign: {
+          senderId: 'ca654',
+          subject: 'my campaign subject',
+          listIds: 'ca43546',
+          name: 'my campaign',
+          body: 'my campaign body'
+        },
+        authToken: ''
       });
-    });
-
-    context('when the event is not valid', () => {
-      event = {};
-      it('returns an error message', (done) => {
-        respond(event, (err, result) => {
-          expect(result).to.not.exist;
-          expect(err).to.exist;
-          done();
-        });
-      });
-    });
-
-    afterEach(() => {
-      CampaignModel.save.restore();
-    });
+    } catch(error) {
+      // THEN
+      expect(error.message).toBe('[403] Access denied');
+    }
   });
 });
--- campaigns-microservice-new/src/api/createCampaign/handler.ts
+++ campaigns-microservice-new/src/api/createCampaign/handler.ts
@@ -1,20 +1,17 @@
 import debug from '../../lib/logger';
 import decrypt from '../../lib/auth-token-decryptor';
 import { ApiErrors } from '../../lib/errors';
-import { CampaignEvent, CampaignRepository, TokenData, CampaignStatus } from '../../types';
+import { CampaignEvent, CampaignRepository, TokenData, CampaignStatus, Campaign } from '../../types';
 import campaignRepository from '../../repositories/campaign';
 
 export function handlerService(service: CampaignRepository) {
-  return async function handler({ campaign, authToken }: CampaignEvent) {
+  return async function handler({ campaign, authToken }: CampaignEvent): Promise<Campaign> {
     debug('= createCampaign.action', JSON.stringify({ campaign, authToken }));
-    if (!authToken) {
-      throw new Error('[403] Access denied'); // It'll be moved to Authentication lambda
-    }
-
     try {
       const {sub}: TokenData = decrypt(authToken);
       campaign.userId = sub;
       campaign.status = CampaignStatus.DRAFT;
+
       await service.save(campaign);
       return campaign;
     } catch (error) {
