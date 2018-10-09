b9beded11b068a27ff20f480a42f098589202bdf
Rhino <rhinoandre@gmail.com>
2018-10-09 11:03:25 -0300

    [4519] Adding delete method on types

--- campaigns-microservice-new/src/types/CampaignRepository.ts
+++ campaigns-microservice-new/src/types/CampaignRepository.ts
@@ -2,7 +2,8 @@ import { Campaign } from './Campaign';
 
 export interface CampaignRepository {
   save: (campaign: Campaign) => Promise<Campaign>,
-  edit: (id:string, campaign: Campaign) => Promise<Campaign>,
+  edit: (id: string, campaign: Campaign) => Promise<Campaign>,
   get: (id: string) => Promise<Campaign>,
-  list: (userId: string) => Promise<Array<Campaign>>
+  list: (userId: string) => Promise<Array<Campaign>>,
+  delete: (userId: string, id: string) => Promise<boolean>
 }
