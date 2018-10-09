1c355a183d42cedef2084f171e9dd97a8e51bec6
Rhino <rhinoandre@gmail.com>
2018-10-09 11:05:50 -0300

    [4519] Tests for delete DynamoService

--- campaigns-microservice-new/src/lib/dynamo.spec.ts
+++ campaigns-microservice-new/src/lib/dynamo.spec.ts
@@ -1,14 +1,14 @@
 import dynamoService, {client} from './dynamo';
 
 describe('DynamoService', () => {
-  it('should save the provided object', async () => {
-    // GIVEN
-    spyOn(client, 'put').and.callFake((param, callback) => {callback()});
-
-    // WHEN
-    const result = await dynamoService.put({
-      TableName: 'someTableName',
-      Item: {
+  describe('Create', () => {
+    it('should save the provided object', async () => {
+      // GIVEN
+      process.env.CAMPAIGN_TABLE = 'companyTable';
+      spyOn(client, 'put').and.callFake((param, callback) => {callback()});
+  
+      // WHEN
+      const result = await dynamoService.put({
         id: 'someId',
         usreId: 'my-user-id',
         senderId: 'ca654',
@@ -22,35 +22,28 @@ describe('DynamoService', () => {
         sentAt: 0,
         createdAt: 0,
         scheduleAt: 0
-      }
-    });
+      });
+  
+      // THEN
+      expect(result).toEqual({
+        id: 'someId',
+        usreId: 'my-user-id',
+        senderId: 'ca654',
+        segmentId: 'anotherId',
+        subject: 'my campaign subject',
+        listId: 'ca43546',
+        name: 'my campaign',
+        body: 'my campaign body',
+        template: 'htmlBody',
+        isUpToDate: true,
+        sentAt: 0,
+        createdAt: 0,
+        scheduleAt: 0
+      });
 
-    // THEN
-    expect(result).toEqual({
-      id: 'someId',
-      usreId: 'my-user-id',
-      senderId: 'ca654',
-      segmentId: 'anotherId',
-      subject: 'my campaign subject',
-      listId: 'ca43546',
-      name: 'my campaign',
-      body: 'my campaign body',
-      template: 'htmlBody',
-      isUpToDate: true,
-      sentAt: 0,
-      createdAt: 0,
-      scheduleAt: 0
-    });
-  });
-
-  it('should save the provided object', async () => {
-    // GIVEN
-    spyOn(client, 'put').and.callFake((param, callback) => {callback('error')});
-
-    // WHEN
-    try {
-      await dynamoService.put({
-        TableName: 'someTableName',
+      // AND
+      expect(client.put).toHaveBeenCalledWith({
+        TableName: 'companyTable',
         Item: {
           id: 'someId',
           usreId: 'my-user-id',
@@ -66,11 +59,83 @@ describe('DynamoService', () => {
           createdAt: 0,
           scheduleAt: 0
         }
-      });
-    } catch (error) {
-      // THEN
-      expect(error).toBe('error');
-    }
+      },
+      expect.any(Function));
+    });
+  
+    it('should raise the error if something goes wrong in Item creation', async () => {
+      // GIVEN
+      spyOn(client, 'put').and.callFake((param, callback) => {callback('error on AWS API')});
+  
+      // WHEN
+      try {
+        await dynamoService.put({
+          id: 'someId',
+          usreId: 'my-user-id',
+          senderId: 'ca654',
+          segmentId: 'anotherId',
+          subject: 'my campaign subject',
+          listId: 'ca43546',
+          name: 'my campaign',
+          body: 'my campaign body',
+          template: 'htmlBody',
+          isUpToDate: true,
+          sentAt: 0,
+          createdAt: 0,
+          scheduleAt: 0
+        });
 
+      } catch (error) {
+        // THEN
+        expect(error).toBe('error on AWS API');
+
+        // AND
+        expect(client.put).toHaveBeenCalledWith({
+          TableName: 'companyTable',
+          Item: {
+            id: 'someId',
+            usreId: 'my-user-id',
+            senderId: 'ca654',
+            segmentId: 'anotherId',
+            subject: 'my campaign subject',
+            listId: 'ca43546',
+            name: 'my campaign',
+            body: 'my campaign body',
+            template: 'htmlBody',
+            isUpToDate: true,
+            sentAt: 0,
+            createdAt: 0,
+            scheduleAt: 0
+          }
+        },
+        expect.any(Function));
+      }
+    });
   });
+
+  describe('Delete', () => {
+    it('should delete the campaign', async () => {
+      // GIVEN
+      spyOn(client, 'delete').and.callFake((param, callback) => callback());
+
+      // WHEN
+      const result = await dynamoService.delete('someId','my-user-id');
+
+      // THEN
+      expect(result).toBe(true);
+    });
+
+    it('should raise the error if AWS Dynamo lib throws an error', async () => {
+      // GIVEN
+      spyOn(client, 'delete').and.callFake((param, callback) => callback('error'));
+
+      // WHEN
+      try {
+        await dynamoService.delete('someId','my-user-id');
+      } catch (error) {
+        // THEN
+        expect(error).toBe('error');
+      }
+    });
+  })
 });
--- campaigns-microservice-new/src/lib/dynamo.ts
+++ campaigns-microservice-new/src/lib/dynamo.ts
@@ -1,31 +1,61 @@
 import { DynamoDB } from 'aws-sdk';
-import { DocumentClient, PutItemInput, GetItemInput, ScanInput } from 'aws-sdk/clients/dynamodb';
-import { DatabaseService } from '../types';
+import { DocumentClient, GetItemInput, ScanInput } from 'aws-sdk/clients/dynamodb';
+import { DatabaseService, Campaign } from '../types';
 
+export const HASH_KEY = 'userId';
+export const RANGE_KEY = 'id';
 export const client = new DynamoDB.DocumentClient({
   region: process.env.region
 });
 
 export function dynamoFactory(client: DocumentClient): DatabaseService {
   return {
-    put (params: PutItemInput) {
+    put (campaign: Campaign) {
       return new Promise((resolve, reject) => {
-        client.put(params, (err) => {
+        client.put(buildDynamoParamObject(campaign), (err) => {
           if (err) {
             reject(err);
             return;
           }
-          resolve(params.Item);
+          resolve(campaign);
         });
       });
     },
     async get(params: GetItemInput) {
-      throw new Error('[500] Method not implemented')
+      throw new Error('[500] Method not implemented');
     }, // Soon it'll be implemented
     async list(params: ScanInput) {
-      throw new Error('[500] Method not implemented')
+      throw new Error('[500] Method not implemented');
+    },
+    async delete(hash: string, range: string) {
+      return new Promise((resolve, reject) => {
+        client.delete(buildDynamoKeyObject(hash, range), (error) => {
+          if (error) {
+            reject(error)
+          }
+          resolve(true)
+        })
+      });
     }
   }
 }
+
+function buildDynamoParamObject(value) {
+  return {
+    TableName: process.env.CAMPAIGN_TABLE,
+    Item: value
+  }
+}
+
+function buildDynamoKeyObject(hash: string, range: string) {
+  return {
+    TableName: process.env.CAMPAIGN_TABLE,
+    Key: {
+      [HASH_KEY]: hash,
+      [RANGE_KEY]: range
+    }
+  };
+}
+
 
 export default dynamoFactory(client);
