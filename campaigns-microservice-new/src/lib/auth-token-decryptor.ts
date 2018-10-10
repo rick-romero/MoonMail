0ea89b0b5989574446a632d8701d3fb492f08b5a
Rhino <rhinoandre@gmail.com>
2018-10-10 09:04:17 -0300

    Remove comments

--- campaigns-microservice-new/src/lib/auth-token-decryptor.ts
+++ campaigns-microservice-new/src/lib/auth-token-decryptor.ts
@@ -2,8 +2,6 @@ import * as jwt from 'jsonwebtoken';
 import { TokenData } from '../types';
 import ApiErrors from './errors';
 
-// import { GetUserAccountService } from './get-user-account-service';
-
 export default function decrypt(authToken: string): TokenData {
   if (!authToken) {
     throw ApiErrors.errors.accessDenied; // It'll be moved to Authentication lambda
@@ -14,17 +12,3 @@ export default function decrypt(authToken: string): TokenData {
 
   return jwt.verify(tokenWithoutBearer, cert, { algorithms: ['RS256'] });
 }
-
-// const getUserContext = (userId) => GetUserAccountService.getAccount(userId);
-// const getUserContextMock = () => ({ id: 'my-user-id', plan: 'gold' })
-
-
-// function getUserContextFn() {
-//   if (process.env.NODE_ENV === 'test') {
-//     return getUserContextMock;
-//   }
-//   return getUserContext;
-// }
-
-// const _getUserContext = getUserContextFn();
-// export { _getUserContext as getUserContext };
