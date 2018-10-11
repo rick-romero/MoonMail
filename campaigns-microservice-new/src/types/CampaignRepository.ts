4e4301f92fc881ca78bb2eb17ddb585686225762
Rhino <rhinoandre@gmail.com>
2018-10-11 14:11:02 -0300

    [4519] Change from edit to update on CampaignRepository

--- campaigns-microservice-new/src/types/CampaignRepository.ts
+++ campaigns-microservice-new/src/types/CampaignRepository.ts
@@ -2,7 +2,7 @@ import { Campaign } from './Campaign';
 
 export interface CampaignRepository {
   save: (campaign: Campaign) => Promise<Campaign>,
-  edit: (campaign: Campaign, userId: string, id: string) => Promise<Campaign>,
+  update: (campaign: Campaign, userId: string, id: string) => Promise<Campaign>,
   get: (id: string) => Promise<Campaign>,
   list: (userId: string) => Promise<Array<Campaign>>,
   delete: (userId: string, id: string) => Promise<boolean>
